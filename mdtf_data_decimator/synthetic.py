40c28c30b96ef0e07f063dd777b7737b569c503f
John Krasting <John.Krasting@noaa.gov>
2020-11-28 13:57:38 -0500

    Synthetic updates
    - Added daily and hourly time axes generation
    - Added logic to save time as either 32-bit int or 64-bit float
    - New subroutine for printing variable statistics

--- mdtf_data_decimator/synthetic.py
+++ mdtf_data_decimator/synthetic.py
@@ -14,6 +14,139 @@ import numpy as np
 from .coarseners import *
 
 
+def dataset_stats(filename, var=None):
+    """Prints statistics and attributes for a NetCDF file
+
+    Parameters
+    ----------
+    filename : str, path-like
+        Path to NetCDF file to analyze
+    var : str, optional
+        Variable to analyze (None prints a list of variables), by default None
+    """
+    dset = xr.open_dataset(filename)
+    if var is None:
+        print(list(dset.variables))
+    else:
+        means = dset[var].mean(axis=(0, -2, -1)).values
+        stds = dset[var].std(axis=(0, -2, -1)).values
+        print(dset[var].attrs)
+        print(means, stds)
+    dset.close()
+
+
+def generate_daily_time_axis(startyear, nyears):
+    """Construct a daily noleap time dimension with associated bounds
+
+    Parameters
+    ----------
+    startyear : int
+        Start year for requested time axis
+    nyears : int
+        Number of years in requested time axis
+
+    Returns
+    -------
+    xarray.DataArray
+        time and time_bnds xarray DataArray types
+    """
+
+    ndays = 365 * nyears
+    daysinmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    months = list(np.arange(1, 13))
+    days = [np.arange(1, daysinmonth[n] + 1) for n, x in enumerate(months)]
+    days = [item for sublist in days for item in sublist]
+    days = days * nyears
+    months = [[months[n]] * daysinmonth[n] for n, x in enumerate(months)]
+    months = [item for sublist in months for item in sublist]
+    months = months * nyears
+    years = list(np.arange(startyear, startyear + nyears))
+    years = [[years[x]] * 365 for x in range(0, len(years))]
+    years = [item for sublist in years for item in sublist]
+    timetuple = list(zip(years, months, days))
+    times = [cftime.DatetimeNoLeap(*x, calendar="noleap") for x in timetuple]
+    time = xr.DataArray(
+        times,
+        dims={"time": times},
+        coords={"time": (times)},
+        attrs={"long_name": "time", "bounds": "time_bnds"},
+    )
+
+    timetuple = list(zip(years, months, days)) + [(startyear + nyears, 1, 1)]
+    bounds = [cftime.DatetimeNoLeap(*x, calendar="noleap") for x in timetuple]
+    bounds = list(zip(bounds[0:-1], bounds[1::]))
+    nbnds = np.array([0, 1])
+    time_bnds = xr.DataArray(
+        bounds,
+        coords=(time, ("nbnds", nbnds)),
+        attrs={"long_name": "time interval endpoints"},
+    )
+
+    return time, time_bnds
+
+
+def generate_hourly_time_axis(startyear, nyears, dhour):
+    """Construct an hourly noleap time dimension with associated bounds
+
+    Parameters
+    ----------
+    startyear : int
+        Start year for requested time axis
+    nyears : int
+        Number of years in requested time axis
+    dhour : int
+        Delta skip for hours (e.g. 1 hour, 3 hours, 6 hours)
+
+    Returns
+    -------
+    xarray.DataArray
+        time and time_bnds xarray DataArray types
+    """
+
+    ndays = 365 * nyears
+    nhours = int(24 / dhour)
+    hours = list(np.arange(0, 24, dhour))
+    hours = hours * 365 * nyears
+    daysinmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    months = list(np.arange(1, 13))
+    days = [np.arange(1, daysinmonth[n] + 1) for n in range(0, len(months))]
+    days = [item for sublist in days for item in sublist]
+    days = [item for item in days for i in range(nhours)]
+    days = days * nyears
+    months = [[months[n]] * daysinmonth[n] for n, x in enumerate(months)]
+    months = [item for sublist in months for item in sublist]
+    months = [item for item in months for i in range(nhours)]
+    months = months * nyears
+    years = list(np.arange(startyear, startyear + nyears))
+    years = [[years[x]] * 365 for x in range(0, len(years))]
+    years = [item for sublist in years for item in sublist]
+    years = [item for item in years for i in range(nhours)]
+    timetuple = list(zip(years, months, days, hours))
+    times = [cftime.DatetimeNoLeap(*x, calendar="noleap") for x in timetuple]
+    time = xr.DataArray(
+        times,
+        dims={"time": times},
+        coords={"time": (times)},
+        attrs={"long_name": "time", "bounds": "time_bnds"},
+    )
+
+    timetuple = (
+        [(startyear, 1, 1, 0)]
+        + list(zip(years, months, days, hours))
+        + [(startyear + nyears, 1, 1, dhour)]
+    )
+    bounds = [cftime.DatetimeNoLeap(*x, calendar="noleap") for x in timetuple]
+    bounds = list(zip(bounds[0:-1], bounds[1::]))[0:-1]
+    nbnds = np.array([0, 1])
+    time_bnds = xr.DataArray(
+        bounds,
+        coords=(time, ("nbnds", nbnds)),
+        attrs={"long_name": "time interval endpoints"},
+    )
+
+    return time, time_bnds
+
+
 def generate_monthly_time_axis(startyear, nyears, format="ncar"):
     """Construct a monthly noleap time dimension with associated bounds
 
@@ -63,7 +196,9 @@ def generate_monthly_time_axis(startyear, nyears, format="ncar"):
     return time, time_bnds
 
 
-def generate_ncar_dataset(stats, dlon, dlat, startyear, nyears, varname, attrs={}):
+def generate_ncar_dataset(
+    stats, dlon, dlat, startyear, nyears, varname, timeres="mon", attrs={}
+):
     """Generates xarray dataset of syntheic data in NCAR format
 
     Parameters
@@ -94,7 +229,18 @@ def generate_ncar_dataset(stats, dlon, dlat, startyear, nyears, varname, attrs={
     lon = ds.lon
     xyshape = (len(ds["lat"]), len(ds["lon"]))
 
-    time, time_bnds = generate_monthly_time_axis(startyear, nyears)
+    if timeres == "mon":
+        time, time_bnds = generate_monthly_time_axis(startyear, nyears)
+    elif timeres == "day":
+        time, time_bnds = generate_daily_time_axis(startyear, nyears)
+    elif timeres == "3hr":
+        time, time_bnds = generate_hourly_time_axis(startyear, nyears, 3)
+    elif timeres == "1hr":
+        time, time_bnds = generate_hourly_time_axis(startyear, nyears, 1)
+    else:
+        print(timeres)
+        raise ValueError("Unknown time resolution requested")
+
     ds["time"] = time
     ds["time_bnds"] = time_bnds
 
@@ -380,7 +526,7 @@ def ncar_hybrid_coord():
     return lev, hyam, hybm
 
 
-def write_to_netcdf(dset_out, outfile):
+def write_to_netcdf(dset_out, outfile, time_dtype="float"):
     """Writes xarray dataset to NetCDF with proper encodings
 
     Parameters
@@ -393,11 +539,19 @@ def write_to_netcdf(dset_out, outfile):
     encoding = {}
     for var in list(dset_out.variables):
         if var == "time":
-            dset_out[var].encoding["dtype"] = "i4"
             dset_out[var].encoding["units"] = "days since 1975-01-01"
+            if time_dtype == "float":
+                dset_out[var].encoding["dtype"] = "float64"
+                dset_out[var].encoding["_FillValue"] = 1.0e20
+            elif time_dtype == "int":
+                dset_out[var].encoding["dtype"] = "i4"
         elif var == "time_bnds":
-            dset_out[var].encoding["dtype"] = "i4"
             dset_out[var].encoding["units"] = "days since 1975-01-01"
+            if time_dtype == "float":
+                dset_out[var].encoding["dtype"] = "float64"
+                dset_out[var].encoding["_FillValue"] = 1.0e20
+            elif time_dtype == "int":
+                dset_out[var].encoding["dtype"] = "i4"
         elif var == "date":
             dset_out[var].encoding["dtype"] = "i4"
         elif "float" in str(dset_out[var].dtype):
