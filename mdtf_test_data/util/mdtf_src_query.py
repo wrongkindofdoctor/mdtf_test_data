15d65b85141c4ad286d04fcd11a37abfa285aff3
John Krasting <John.Krasting@noaa.gov>
2021-08-11 08:52:46 -0400

    Apply Black formatting to source query script

--- mdtf_test_data/util/mdtf_src_query.py
+++ mdtf_test_data/util/mdtf_src_query.py
@@ -5,7 +5,7 @@ from synthetic.synthetic_data import dataset_stats
 
 if __name__ == "__main__":
     filename = sys.argv[1]
-    var = sys.argv[2] if len(sys.argv)>=3 else None
-    limit = sys.argv[3] if len(sys.argv)>=4 else None
+    var = sys.argv[2] if len(sys.argv) >= 3 else None
+    limit = sys.argv[3] if len(sys.argv) >= 4 else None
 
-    dataset_stats(filename,var,limit)
+    dataset_stats(filename, var, limit)
