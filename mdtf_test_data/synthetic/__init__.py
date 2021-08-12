0f4f0db8026b23701d0e32516c3722e3cbb96754
John Krasting <John.Krasting@noaa.gov>
2021-08-12 19:38:31 -0400

    Refactored vertical coordinate routines

--- mdtf_test_data/synthetic/__init__.py
+++ mdtf_test_data/synthetic/__init__.py
@@ -3,10 +3,8 @@
 from .synthetic_data import (
     dataset_stats,
     generate_synthetic_dataset,
-    gfdl_vertical_coord,
-    gfdl_plev19_vertical_coord,
-    ncar_hybrid_coord,
     write_to_netcdf,
 )
 
 from . import time
+from . import vertical
